fcabdafdd7313b98f3f807fafc16913e0e2d4351
viaylwen <novialita.a@gmail.com>
2021-07-07 16:18:22 +0700

    altered

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
-    validate :username, presence: true,
+    validates :username, presence: true,
                 uniqueness: { case_sensitive: false },
                 length: {minimum: 3}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validate :email, presence: true,
+    validates :email, presence: true,
                 uniqueness: { case_sensitive: false },
                 length: {maximum: 105},
                 format: { with: VALID_EMAIL_REGEX }
