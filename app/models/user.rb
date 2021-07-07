8d025892202943a5853e83cd657d45cb37b9f7b3
viaylwen <novialita.a@gmail.com>
2021-07-07 15:56:51 +0700

    user-to-articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     validate :username, presence: true,
                 uniqueness: { case_sensitive: false },
                 length: {minimum: 3}
