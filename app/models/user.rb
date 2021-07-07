8a76a894d99edfcdf8eb06ecd013a6762ce2488f
viaylwen <novialita.a@gmail.com>
2021-07-07 14:48:33 +0700

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    validate :username, presence: true,
+                uniqueness: { case_sensitive: false },
+                length: {minimum: 3}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validate :email, presence: true,
+                uniqueness: { case_sensitive: false },
+                length: {maximum: 105},
+                format: { with: VALID_EMAIL_REGEX }
+
 end
