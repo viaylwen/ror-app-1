c29cd364b72ce47d1094d68702970830799b0ec1
viaylwen <novialita.a@gmail.com>
2021-07-07 16:53:08 +0700

    pwd hashing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_075223) do
+ActiveRecord::Schema.define(version: 2021_07_07_092328) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2021_07_07_075223) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
+    t.string "password_digest"
   end
 
 end
